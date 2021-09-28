424dbcdba8500d9127ac5e2e5d0a669deac5c5b8
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-09-28 15:19:22 +1000

    Add test console.logs to see if routes work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,31 @@
 const router = require("express").Router();
 const db = require("../models")
 
+//Get latest workout
 router.get("/workouts", (req, res) => {
+    console.log("Success1");
+
 
 })
 
-router.get("/workouts/:id", (req, res) => {
-    
+// Add exercise to workout
+router.put("/workouts/:id", (req, res) => {
+    console.log("Success2");
+
+
 })
 
+//Create new workout
 router.post("/workouts", (req, res) => {
-    
+    console.log("Success3");
+
+
 })
 
+//Gets all workouts and puts in in graph
 router.get("/workouts/range", (req, res) => {
+    console.log("Success4");
+
     
 })
 
