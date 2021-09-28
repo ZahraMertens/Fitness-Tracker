081e09e382bae2ab052f52b0a0da2041377e6c84
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-09-28 14:39:31 +1000

    Add all required api routes fetched in api.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
 const db = require("../models")
 
+router.get("/workouts", (req, res) => {
 
+})
+
+router.get("/workouts/:id", (req, res) => {
+    
+})
+
+router.post("/workouts", (req, res) => {
+    
+})
+
+router.get("/workouts/range", (req, res) => {
+    
+})
 
 
 module.exports = router;
