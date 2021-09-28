a6db4db2bc290f6020604b28afb424ef7363930d
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-09-28 14:45:16 +1000

    Add schema structure for workouts

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,39 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-
+    day: {
+        type: Date,
+        default: Date.now(),
+    },
+    exercise: [
+        {
+            type: {
+                type: String,
+                required: "Exercise type is required!",
+                trim: true,
+            },
+            name: {
+                type: String,
+                required: "Exercise name is required!",
+                trim: true,
+            },
+            duration: {
+                type: Number,
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            }
+        }
+    ]
 })
 
 const Workout = mongoose.model("Workout", workoutSchema);
